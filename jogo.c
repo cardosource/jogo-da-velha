b8bb99cbca621f913b5c28e4bf5f6bbbd3c98490
cardoso <jefersoncardoso.dev@gmail.com>
2022-11-17 00:16:59 -0400

    base inicial
    Iniciando programação do jogo

---
+++ jogo.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string>
+#include <iostream>
+#include <locale.h>
+
+
+
+int main(){
+    setlocale(LC_ALL,"");
+      
+ 
+    
+
+    return 0;
+}
