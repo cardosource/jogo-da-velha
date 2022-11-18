954738667175587a46206d5c588262d3958379a9
cardoso <jefersoncardoso.dev@gmail.com>
2022-11-18 00:02:30 -0400

    metodo auxiliar jogo

--- jogo.cpp
+++ jogo.cpp
@@ -5,6 +5,16 @@
 #include <locale.h>
 
 
+void instrucoes(){
+    cout << "\n-Mapa de posições\n\n";
+    cout << "7 | 8 | 9\n";
+    cout << "__|___|___\n";
+    cout << "4 | 5 | 6\n";
+    cout << "__|___|___\n";
+    cout << "1 | 2 | 3\n\n";
+
+
+}
 
 int main(){
     setlocale(LC_ALL,"");
